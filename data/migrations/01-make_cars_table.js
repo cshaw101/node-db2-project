29e50629c09b2f62b76999a0960ece80bb3597a6
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-28 17:10:29 -0400

    migrations are working correctly

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+return knex.schema.createTable('cars', tbl => {
+  tbl.increments('id');
+  tbl.text('vin', 50).unique().notNullable();
+  tbl.text('make',30).notNullable();
+  tbl.integer('mileage').notNullable();
+  tbl.text('title');
+  tbl.text('transmission');
+})
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
+
