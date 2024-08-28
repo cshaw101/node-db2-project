1b694640204ffc7cefc91d10ea1c83b2e5686412
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-28 17:18:44 -0400

    routers are wired

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,26 @@
-// DO YOUR MAGIC
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+res.json('get is working')
+})
+
+router.get('/:id', (req, res) => {
+res.json('get :id is working')
+})
+
+router.post('/', (req, res) => {
+res.json('post is working')
+})
+
+
+
+
+
+
+
+
+module.exports = router;
+
+// - `[GET] /api/cars` returns an array of cars sorted by id (or an empty array if there aren't any).
+//   - `[GET] /api/cars/:id` returns a car by the given id.
+//   - `[POST] /api/cars` returns the created car.
