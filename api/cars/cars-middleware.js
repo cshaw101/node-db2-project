fd9a783388028c03e47be7c7d5d88bd93bcf6269
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-30 14:51:16 -0400

    just need final middleware done

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,10 +1,42 @@
-const checkCarId = (req, res, next) => {
+const Cars = require('./cars-model');
+
+const checkCarId = async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const car = await Cars.getById(req.params.id)
+    if (!car) {
+      next({ status: 404, message: 'not found' })
+    }else {
+      req.car = car;
+      next()
+    }
+  }catch(err) {
+  res.status(500).json({message: 'middle ware isnt working'})
+  next()
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { vin, make, model, mileage } = req.body;
+
+  const requiredFields = [
+    { field: 'vin', value: vin },
+    { field: 'make', value: make },
+    { field: 'model', value: model },
+    { field: 'mileage', value: mileage }
+  ];
+
+  const missingField = requiredFields.find(field => !field.value);
+
+  if (missingField) {
+    return res.status(400).json({ 
+      message: `${missingField.field} is missing`
+    });
+  }
+
+  next();
+};
+
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
@@ -12,4 +44,12 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Cars = require('./cars-model.js')
+const md = require('./cars-middleware')
 
 router.get('/', async (req, res, next) => {
     try {
@@ -10,7 +11,7 @@ router.get('/', async (req, res, next) => {
     }
   });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', md.checkCarId, async (req, res) => {
 try {
     const car = await Cars.getById(req.params.id)
     res.json(car)
@@ -19,21 +20,17 @@ next(err)
 }
 })
 
-router.post('/', async (req, res, next) => {
-    try {
-      const carData = req.body;
-  
-      const { vin, make, mileage } = carData;
-      if (!vin || !make || !mileage) {
-        return res.status(400).json({ message: 'VIN, make, and mileage are required.' });
-      }
-      const car = await Cars.create(carData);
+router.post('/', md.checkCarPayload, async (req, res, next) => {
+  try {
+    const carData = req.body;
+    const car = await Cars.create(carData);
 
-      res.status(201).json(car);
-    } catch (err) {
-      next(err);  
-    }
-  });
+    res.status(201).json(car);
+  } catch (err) {
+    next(err);  
+  }
+});
+
 
 router.use((err, req, res, next) => { //eslint-disable-line
     res.status(err.status|| 500).json({
